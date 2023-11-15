4161fc86b7b2143cfc09c10329b9a92ca80cd3bf
salskhal <olaniyisal@gmail.com>
2023-11-15 08:48:44 +0100

    feat/ added the CTA section content

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,16 +5,17 @@ type ButtonProps = {
   type: "button" | "submit";
   icon: string;
   varient: string;
+  full?: boolean;
 };
 
-const Button = ({ title, type, icon, varient }: ButtonProps) => {
+const Button = ({ title, type, icon, varient, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${varient}`}
+      className={`flexCenter gap-3 rounded-full border ${varient} ${full && "w-full"}`}
     >
       {icon && <Image src={icon} alt="icon" width={20} height={20} />}{" "}
-      <label>{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   );
 };
